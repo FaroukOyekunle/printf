e1ca53fee5d9beed1c75b4a901fdf8a16eb47de3
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2023-06-24 13:50:46 -0700

    Write a function that produces output according to a format. according with the betty coding style

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* _printf - function that produces output according to a format.
+*
+* Description: function that produces output according to a format.
+*
+* @format: String format
+*
+* Return: number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+	int format_position, number_of_characters_printed;
+	va_list all_parameters;
+
+	if (format == 0)
+		return (-1);
+	else if (format[0] == '%' && format[1] == '\0')
+		return (-1);
+	va_start(all_parameters, format);
+	format_position = number_of_characters_printed = 0;
+	while (format[format_position] != '\0')
+	{
+		if (format[format_position] == '%' && format[format_position + 1] != '\0')
+			number_of_characters_printed += process_flag(format[format_position + 1],
+					all_parameters);
+		if (!is_flag(format[format_position], format[format_position + 1]))
+		{
+			number_of_characters_printed += _putchar(format[format_position]);
+			format_position++;
+		}
+		else
+			format_position += 2;
+	}
+	va_end(all_parameters);
+	return (number_of_characters_printed);
+}
